f7dd6e71426d75bcb62b02c933d7326fb8c2e8a5
Masahiro Honma <hiratara@cpan.org>
2017-07-11 08:49:15 +0900

    Implement Audio object

--- src/Web/RTBBidder/Types.hs
+++ src/Web/RTBBidder/Types.hs
@@ -203,10 +203,74 @@ instance AESON.ToJSON Video where
     , "ext" .= videoExt
     ]
 
-data Audio = Audio { audioX :: Maybe () } deriving (Show, Eq)
-$(AESON.deriveJSON AESON.defaultOptions {
-    AESON.fieldLabelModifier = map toLower . drop 5
-  } ''Audio)
+data Audio = Audio
+  { audioMimes :: [TX.Text]
+  , audioMinduration :: Maybe Int
+  , audioMaxduration :: Maybe Int
+  , audioProtocols :: Maybe Int
+  , audioStartdelay :: Maybe Int
+  , audioSequence :: Maybe Int
+  , audioBattr :: [Int]
+  , audioMaxextended :: Maybe Int
+  , audioMinbitrate :: Maybe Int
+  , audioMaxbitrate :: Maybe Int
+  , audioDelivery :: [Int]
+  , audioCompanionad :: [Banner]
+  , audioApi :: [Int]
+  , audioCompaniontype :: [Int]
+  , audioMaxseq :: Maybe Int
+  , audioFeed :: Maybe Int
+  , audioStitched :: Maybe Int
+  , audioNvol :: Maybe Int
+  , audioExt :: Maybe AESON.Value
+  } deriving (Show, Eq)
+
+instance AESON.FromJSON Audio where
+  parseJSON = AESON.withObject "audio" $ \o -> do
+    audioMimes <- o .: "mimes"
+    audioMinduration <- o .:? "minduration"
+    audioMaxduration <- o .:? "maxduration"
+    audioProtocols <- o .:? "protocols"
+    audioStartdelay <- o .:? "startdelay"
+    audioSequence <- o .:? "sequence"
+    audioBattr <- o .:? "battr" .!= []
+    audioMaxextended <- o .:? "maxextended"
+    audioMinbitrate <- o .:? "minbitrate"
+    audioMaxbitrate <- o .:? "maxbitrate"
+    audioDelivery <- o .:? "delivery" .!= []
+    audioCompanionad <- o .:? "companionad" .!= []
+    audioApi <- o .:? "api" .!= []
+    audioCompaniontype <- o .:? "companiontype" .!= []
+    audioMaxseq <- o .:? "maxseq"
+    audioFeed <- o .:? "feed"
+    audioStitched <- o .:? "stitched"
+    audioNvol <- o .:? "nvol"
+    audioExt <- o .:? "ext"
+
+    return Audio{..}
+
+instance AESON.ToJSON Audio where
+  toJSON Audio{..} = AESON.object
+    [ "mimes" .= audioMimes
+    , "minduration" .= audioMinduration
+    , "maxduration" .= audioMaxduration
+    , "protocols" .= audioProtocols
+    , "startdelay" .= audioStartdelay
+    , "sequence" .= audioSequence
+    , "battr" .= audioBattr
+    , "maxextended" .= audioMaxextended
+    , "minbitrate" .= audioMinbitrate
+    , "maxbitrate" .= audioMaxbitrate
+    , "delivery" .= audioDelivery
+    , "companionad" .= audioCompanionad
+    , "api" .= audioApi
+    , "companiontype" .= audioCompaniontype
+    , "maxseq" .= audioMaxseq
+    , "feed" .= audioFeed
+    , "stitched" .= audioStitched
+    , "nvol" .= audioNvol
+    , "ext" .= audioExt
+    ]
 
 data Native = Native { nativeX :: Maybe () } deriving (Show, Eq)
 $(AESON.deriveJSON AESON.defaultOptions {
