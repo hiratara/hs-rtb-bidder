14bf86c9fbdf52f875a4f520dafe76e3efe89253
Masahiro Honma <hiratara@cpan.org>
2017-07-12 08:37:14 +0900

    Implement Site object

--- src/Web/RTBBidder/Types.hs
+++ src/Web/RTBBidder/Types.hs
@@ -444,10 +444,76 @@ instance AESON.ToJSON Imp where
     , "ext" .= impExt
     ]
 
-data Site = Site { siteX :: Maybe () } deriving (Show, Eq)
+data Publisher = Publisher
+  { pubX :: Maybe ()
+  } deriving (Show, Eq)
 $(AESON.deriveJSON AESON.defaultOptions {
-    AESON.fieldLabelModifier = map toLower . drop 4
-  } ''Site)
+    AESON.fieldLabelModifier = map toLower . drop 3
+  } ''Publisher)
+
+data Content = Content
+  { cntX :: Maybe ()
+  } deriving (Show, Eq)
+$(AESON.deriveJSON AESON.defaultOptions {
+    AESON.fieldLabelModifier = map toLower . drop 3
+  } ''Content)
+
+data Site = Site
+  { siteId :: Maybe TX.Text
+  , siteName :: Maybe TX.Text
+  , siteDomain :: Maybe TX.Text
+  , siteCat :: [TX.Text]
+  , siteSectionCat :: [TX.Text]
+  , sitePagecat :: [TX.Text]
+  , sitePage :: Maybe TX.Text
+  , siteRef :: Maybe TX.Text
+  , siteSearch :: Maybe TX.Text
+  , siteMobile :: Maybe Int
+  , sitePrivacypolicy :: Maybe Int
+  , sitePublisher :: Maybe Publisher
+  , siteContent :: Maybe Content
+  , siteKeywords :: Maybe TX.Text
+  , siteExt :: Maybe AESON.Value
+  } deriving (Show, Eq)
+
+instance AESON.FromJSON Site where
+  parseJSON = AESON.withObject "site" $ \o -> do
+    siteId <- o .:? "id"
+    siteName <- o .:? "name"
+    siteDomain <- o .:? "domain"
+    siteCat <- o .:? "cat" .!= []
+    siteSectionCat <- o .:? "sectioncat" .!= []
+    sitePagecat <- o .:? "pagecat" .!= []
+    sitePage <- o .:? "page"
+    siteRef <- o .:? "ref"
+    siteSearch <- o .:? "search"
+    siteMobile <- o .:? "mobile"
+    sitePrivacypolicy <- o .:? "privacypolicy"
+    sitePublisher <- o .:? "publishser"
+    siteContent <- o .:? "content"
+    siteKeywords <- o .:? "keywords"
+    siteExt <- o .:? "ext"
+
+    return Site{..}
+
+instance AESON.ToJSON Site where
+  toJSON Site{..} = AESON.object
+    [ "id" .= siteId
+    , "name" .= siteName
+    , "domain" .= siteDomain
+    , "cat" .= siteCat
+    , "sectioncat" .= siteSectionCat
+    , "pagecat" .= sitePagecat
+    , "page" .= sitePage
+    , "ref" .= siteRef
+    , "search" .= siteSearch
+    , "mobile" .= siteMobile
+    , "privacypolicy" .= sitePrivacypolicy
+    , "publishser" .= sitePublisher
+    , "content" .= siteContent
+    , "keywords" .= siteKeywords
+    , "ext" .= siteExt
+    ]
 
 data App = App { appX :: Maybe () } deriving (Show, Eq)
 $(AESON.deriveJSON AESON.defaultOptions {
