a1c567abe36f7d93390fecb57dd0bb1167fa11fe
Masahiro Honma <hiratara@cpan.org>
2017-07-12 08:03:51 +0900

    Implement Format object

--- src/Web/RTBBidder/Types.hs
+++ src/Web/RTBBidder/Types.hs
@@ -18,10 +18,35 @@ import qualified Data.Aeson.TH as AESON
 import Data.Char (toLower)
 import qualified Data.Text as TX
 
-data Format = Format { fmtX :: Maybe () } deriving (Show, Eq)
-$(AESON.deriveJSON AESON.defaultOptions {
-    AESON.fieldLabelModifier = map toLower . drop 3
-  } ''Format)
+data Format = Format
+  { fmtW :: Maybe Int
+  , fmtH :: Maybe Int
+  , fmtWratio :: Maybe Int
+  , fmtHratio :: Maybe Int
+  , fmtWmin :: Maybe Int
+  , fmtExt :: Maybe AESON.Value
+  } deriving (Show, Eq)
+
+instance AESON.FromJSON Format where
+  parseJSON = AESON.withObject "format" $ \o -> do
+    fmtW <- o .:? "w"
+    fmtH <- o .:? "h"
+    fmtWratio <- o .:? "wratio"
+    fmtHratio <- o .:? "hratio"
+    fmtWmin <- o .:? "wmin"
+    fmtExt <- o .:? "ext"
+
+    return Format{..}
+
+instance AESON.ToJSON Format where
+  toJSON Format{..} = AESON.object
+    [ "w" .= fmtW
+    , "h" .= fmtH
+    , "wratio" .= fmtWratio
+    , "hratio" .= fmtHratio
+    ,  "wmin" .= fmtWmin
+    , "ext" .= fmtExt
+    ]
 
 data Banner = Banner
   { bannerFormat :: [Format]
