df3b94e46ff82b8872f260c2cc4a864d92eae3c6
Masahiro Honma <hiratara@cpan.org>
2017-07-17 18:46:04 +0900

    Implement Geo object

--- src/Web/RTBBidder/Types.hs
+++ src/Web/RTBBidder/Types.hs
@@ -709,10 +709,59 @@ instance AESON.ToJSON App where
     , "ext" .= appExt
     ]
 
-data Geo = Geo { geoX :: Maybe () } deriving (Show, Eq)
-$(AESON.deriveJSON AESON.defaultOptions {
-    AESON.fieldLabelModifier = map toLower . drop 3
-  } ''Geo)
+data Geo = Geo
+  { geoLat :: Maybe Double
+  , geoLon :: Maybe Double
+  , geoType :: Maybe Int
+  , geoAccuracy :: Maybe Int
+  , geoLastfix :: Maybe Int
+  , geoIpservice :: Maybe Int
+  , geoCountry :: Maybe TX.Text
+  , geoRegion :: Maybe TX.Text
+  , geoRegionfips104 :: Maybe TX.Text
+  , geoMetro :: Maybe TX.Text
+  , geoCity :: Maybe TX.Text
+  , geoZip :: Maybe TX.Text
+  , geoUtcoffset :: Maybe Int
+  , geoExt :: Maybe AESON.Value
+  } deriving (Show, Eq)
+
+instance AESON.FromJSON Geo where
+  parseJSON = AESON.withObject "geo" $ \o -> do
+    geoLat <- o .:? "lat"
+    geoLon <- o .:? "lon"
+    geoType <- o .:? "type"
+    geoAccuracy <- o .:? "accuracy"
+    geoLastfix <- o .:? "lastfix"
+    geoIpservice <- o .:? "ipservice"
+    geoCountry <- o .:? "country"
+    geoRegion <- o .:? "region"
+    geoRegionfips104 <- o .:? "regionfips104"
+    geoMetro <- o .:? "metro"
+    geoCity <- o .:? "city"
+    geoZip <- o .:? "zip"
+    geoUtcoffset <- o .:? "utcoffset"
+    geoExt <- o .:? "ext"
+
+    return Geo{..}
+
+instance AESON.ToJSON Geo where
+  toJSON Geo{..} = AESON.object
+    [ "lat" .= geoLat
+    , "lon" .= geoLon
+    , "type" .= geoType
+    , "accuracy" .= geoAccuracy
+    , "lastfix" .= geoLastfix
+    , "ipservice" .= geoIpservice
+    , "country" .= geoCountry
+    , "region" .= geoRegion
+    , "regionfips104" .= geoRegionfips104
+    , "metro" .= geoMetro
+    , "city" .= geoCity
+    , "zip" .= geoZip
+    , "utcoffset" .= geoUtcoffset
+    , "ext" .= geoExt
+    ]
 
 data Device = Device
   { devUa :: Maybe TX.Text
