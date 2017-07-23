569b1efbc9f77668cdc04bf94967f64aea17f323
Masahiro Honma <hiratara@cpan.org>
2017-07-23 15:03:19 +0900

    Implement User object

--- src/Web/RTBBidder/Types.hs
+++ src/Web/RTBBidder/Types.hs
@@ -868,10 +868,44 @@ instance AESON.ToJSON Device where
     , "ext" .= devExt
     ]
 
-data User = User { userX :: Maybe () } deriving (Show, Eq)
-$(AESON.deriveJSON AESON.defaultOptions {
-    AESON.fieldLabelModifier = map toLower . drop 4
-  } ''User)
+data User = User
+  { userId :: Maybe TX.Text
+  , userBuyeruid :: Maybe TX.Text
+  , userYob :: Maybe Int
+  , userGender :: Maybe TX.Text
+  , userKeywords :: Maybe TX.Text
+  , userCustomdata :: Maybe TX.Text
+  , userGeo :: Maybe Geo
+  , userData :: [Data]
+  , userExt :: Maybe AESON.Value
+  } deriving (Show, Eq)
+
+instance AESON.FromJSON User where
+  parseJSON = AESON.withObject "user" $ \o -> do
+    userId <- o .:? "id"
+    userBuyeruid <- o .:? "buyeruid"
+    userYob <- o .:? "yob"
+    userGender <- o .:? "gender"
+    userKeywords <- o .:? "keywords"
+    userCustomdata <- o .:? "customdata"
+    userGeo <- o .:? "geo"
+    userData <- o .:? "data" .!= []
+    userExt <- o .:? "ext"
+
+    return User{..}
+
+instance AESON.ToJSON User where
+  toJSON User{..} = AESON.object
+    [ "id" .= userId
+    , "buyeruid" .= userBuyeruid
+    , "yob" .= userYob
+    , "gender" .= userGender
+    , "keywords" .= userKeywords
+    , "customdata" .= userCustomdata
+    , "geo" .= userGeo
+    , "data" .= userData
+    , "ext" .= userExt
+    ]
 
 data Source = Source
   { srcFd :: Maybe Int
