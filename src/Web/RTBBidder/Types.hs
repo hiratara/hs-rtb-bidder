d7a9b5edea738df9c2e3c52066b1600c7d24a57d
Masahiro Honma <hiratara@cpan.org>
2017-08-18 16:15:16 +0900

    Implement Bid object

--- src/Web/RTBBidder/Types.hs
+++ src/Web/RTBBidder/Types.hs
@@ -1062,14 +1062,80 @@ instance AESON.ToJSON Request where
     , "ext" .= reqExt
     ]
 
-data Bid = Bid {
-  bidId :: TX.Text
-  , bidImpID :: TX.Text
+data Bid = Bid
+  { bidId :: TX.Text
+  , bidImpid :: TX.Text
   , bidPrice :: Double
+  , bidAdid :: Maybe TX.Text
+  , bidNurl :: Maybe TX.Text
+  , bidAdm :: Maybe TX.Text
+  , bidAdomain :: [TX.Text]
+  , bidBundle :: Maybe TX.Text
+  , bidIurl :: Maybe TX.Text
+  , bidCid :: Maybe TX.Text
+  , bidCrid :: Maybe TX.Text
+  , bidCat :: [TX.Text]
+  , bidAttr :: [Int]
+  , bidApi :: Maybe Int
+  , bidProtocol :: Maybe Int
+  , bidQagmediarating :: Maybe Int
+  , bidDealid :: Maybe TX.Text
+  , bidW :: Maybe Int
+  , bidH :: Maybe Int
+  , bidExp :: Maybe Int
+  , bidExt :: Maybe AESON.Value
   }
-$(AESON.deriveJSON AESON.defaultOptions {
-    AESON.fieldLabelModifier = map toLower . drop 3
-  } ''Bid)
+
+instance AESON.FromJSON Bid where
+  parseJSON = AESON.withObject "bid" $ \o -> do
+    bidId <- o .: "id"
+    bidImpid <- o .: "impid"
+    bidPrice <- o .: "price"
+    bidAdid <- o .:? "adid"
+    bidNurl <- o .:? "nurl"
+    bidAdm <- o .:? "adm"
+    bidAdomain <- o .:? "adomain" .!= []
+    bidBundle <- o .:? "bundle"
+    bidIurl <- o .:? "iurl"
+    bidCid <- o .:? "cid"
+    bidCrid <- o .:? "crid"
+    bidCat <- o .:? "cat" .!= []
+    bidAttr <- o .:? "attr" .!= []
+    bidApi <- o .:? "api"
+    bidProtocol <- o .:? "protocol"
+    bidQagmediarating <- o .:? "qagmediarating"
+    bidDealid <- o .:? "dealid"
+    bidW <- o .:? "w"
+    bidH <- o .:? "h"
+    bidExp <- o .:? "exp"
+    bidExt <- o .:? "ext"
+
+    return Bid{..}
+
+instance AESON.ToJSON Bid where
+  toJSON Bid{..} = AESON.object
+    [ "id" .= bidId
+    , "impid" .= bidImpid
+    , "price" .= bidPrice
+    , "adid" .= bidAdid
+    , "nurl" .= bidNurl
+    , "adm" .= bidAdm
+    , "adomain" .= bidAdomain
+    , "bundle" .= bidBundle
+    , "iurl" .= bidIurl
+    , "cid" .= bidCid
+    , "crid" .= bidCrid
+    , "cat" .= bidCat
+    , "attr" .= bidAttr
+    , "api" .= bidApi
+    , "protocol" .= bidProtocol
+    , "qagmediarating" .= bidQagmediarating
+    , "dealid" .= bidDealid
+    , "w" .= bidW
+    , "h" .= bidH
+    , "exp" .= bidExp
+    , "ext" .= bidExt
+    ]
 
 data SeatBid = SeatBid
   { seatbidBid :: [Bid]
